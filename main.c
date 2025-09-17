9809bb117ee8ec69ecb6b21466b6e4cd61a33583
DomoCro <calicferit@gmail.com>
2025-09-17 14:59:43 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,77 @@
+/* 
+Napišite program u C jeziku koji koristi strukturu za pohranu matrice dimenzija najviše 10 × 10. 
+Struktura treba sadržavati dva cjelobrojna podatka koji predstavljaju broj redaka i broj stupaca (rows, cols) te statički deklarirano dvodimenzionalno polje double a[10][10].
+
+Struktura se mora dinamički zauzeti pomoću malloc ili calloc, dok se njezini članovi ne smiju dodatno dinamički zauzimati – polje za matricu mora biti dio strukture. 
+
+Nakon što se s tipkovnice unesu broj redaka i stupaca (obavezno iz intervala [1,10]), 
+potrebno je popuniti elemente matrice pseudo‑slučajnim realnim brojevima iz intervala [-50, 50], 
+pri čemu se isključivo koristi inicijalizacija generatora >>>>srand(0)<<<<. 
+Funkcijom fillRandom() popuniti matricu pseudo-slučajnim vrijednostima.
+
+Zatim je potrebno pronaći stupac koji sadrži najviše pozitivnih brojeva, ako više stupaca ima jednak broj pozitivnih vrijednosti, 
+treba odabrati onaj s najmanjim indeksom (prvi po redu).
+Program mora ispisati indeks tog stupca te sve njegove vrijednosti, svaku u novom retku. 
+Funkcija bestPositiveCol() provjerava koji stupac ima najviše pozitivnih vrijednosti i ispisuje index stupca i sve vrijednosti tog stupca, svaku u svom redu.
+
+Nakon završetka rada, potrebno je osloboditi dinamički zauzetu memoriju za strukturu, 
+pri čemu funkcija zadužena za oslobađanje mora na standardni izlaz ispisati poruku na engleskom jeziku: memory freed.
+Funkcijom freeMatrix() oslobađa se moemorija i ispisuje poruka "memory freed".
+
+Program organizirati u datoteke (module).
+Ne koristiti VLA polja!
+
+Primjer ulaznih podataka:
+3
+4
+4.44	39.87	-12.19	11.06
+47.29	25.49	-29.89	-7.35
+12.98	-37.29	38.28	10.30
+
+Primjer ispisa:
+REZULTATI:
+0
+4.44
+47.29
+12.98
+
+Napisani kôd ne mijenjati!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 50
+#define MIN -50
+
+typedef struct spremnik {
+	int red;
+	int stupac;
+	int** podaci;
+}MATRICA;
+     
+int main(void){
+    
+    srand(0);
+    
+    M = (MATRICA *)malloc(sizeof(MATRICA ));  
+    M->podaci = (int **)malloc(red * sizeof(int*));
+    for(i=0; i<red; i++){
+        M->podaci[i] = (int*)malloc(stupac * sizeof(int));
+    
+    int red, stupac;
+    
+    scanf("%d", &red);
+    scanf("%d", &stupac);
+    
+    if(red <= 0 || red > 10)
+        if(stupac <= 0 || supac > 10)
+            exit(1);
+    
+    //Ispisati rješenje nakon stringa: REZULTATI:\n!!!
+    printf("REZULTATI:\n");
+    
+    return 0;
+    
+}
